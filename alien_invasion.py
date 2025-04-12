ce2f3166580b7f5d33be407669c00042f3482b54
Pavel Bryzgalov <bryzgalovpaul@yandex.ru>
2025-04-12 21:10:26 +0300

    first_steps

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,1 +1,44 @@
-print('Hello, world')
+import sys
+import pygame
+
+from settings import Settings
+from ship import Ship
+from ben10 import Power
+
+class AlienInvasion:
+    """Класс для управления ресурсами и поведением игры"""
+    def __init__(self):
+        """Инициализирует игру и создаёт игровые ресурсы"""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((
+            self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+        self.power = Power(self)
+
+    def run_game(self):
+        """Запуск основного цикла игры"""
+        while True:
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
+        """Обрабатывает нажатия клавиш и события мыши"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        self.power.blitme()
+
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings():
+    """Класс для хранения всех настроек игры Ilien Invansion"""
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (16, 176, 230)
